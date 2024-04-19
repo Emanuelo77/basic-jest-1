4a3a43609cc450036b3a451cbd8bdbb20630983c
Emanuel crisan <emanuelcrisan778@gmail.com>
2024-04-19 10:39:02 +0200

    projekt arbeit

--- src/calculations/multiply.ts
+++
@@ -1,9 +0,0 @@
-import { Calculation } from "../types/calculation";
-
-export function multiply(calculation: Calculation): number {
-  if (calculation.operator !== '*') {
-    throw new Error('Invalid operator')
-  }
-
-  return calculation.number1 * calculation.number2
-}
--- src/calculations/subtract.ts
+++
@@ -1,9 +0,0 @@
-import { Calculation } from "../types/calculation"
-
-export function subtract(calculation: Calculation): number {
-  if (calculation.operator !== '-') {
-    throw new Error('Invalid operator')
-  }
-
-  return calculation.number1 - calculation.number2
-}
